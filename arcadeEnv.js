b0fdf4868953b9b7e5564880635e020ae2488b5b
Radmehr <radmehrvafadar@gmail.com>
2024-03-12 00:30:17 -0400

    updated arcade model and added helper tools

--- arcadeEnv.js
+++ arcadeEnv.js
@@ -5,7 +5,8 @@ import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
 
 export function arcadeEnvironment(element) {
 
-    const arcadeUrl = new URL('/model1.glb', import.meta.url);
+    const arcadeUrl = new URL('/model2.glb', import.meta.url);
+
 
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, (window.innerHeight/window.innerHeight), 0.1, 1000);
@@ -16,7 +17,9 @@ export function arcadeEnvironment(element) {
 
     renderer.setPixelRatio( window.devicePixelRatio);
     renderer.setSize( window.innerWidth, window.innerHeight)
-    camera.position.setZ(30)
+    
+    camera.position.setZ(10)
+    camera.position.setY(6)
 
 
     const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
@@ -29,16 +32,33 @@ export function arcadeEnvironment(element) {
     const assetLoader = new GLTFLoader();
 
 
+
+    // helpers
+    const gridHelper = new THREE.GridHelper(200, 50);
+    scene.add(gridHelper)
+    
+    const controls = new OrbitControls(camera, renderer.domElement);
+    
+
+
+
+
+
     assetLoader.load(arcadeUrl.href, function(gltf) {
-        const model = gltf.scene;
-        scene.add(model)
+        const model = gltf.scene
+
+        scene.add(model);
+
+        model.rotateY(1.56)
+
+
+
+
     }, undefined, function(error) {
         console.error(error);
     });
 
 
-
- 
     const ambiLight = new THREE.AmbientLight(0xffffff)
     ambiLight.position.add(5,5,5)
 
@@ -52,14 +72,13 @@ export function arcadeEnvironment(element) {
     renderer.render(scene, camera)
    } 
 
-
-   animate()
-
    window.addEventListener('resize', function() {
     camera.aspect = window.innerWidth / window.innerHeight;
     renderer.setSize(window.innerWidth, window.innerHeight)
    })
 
 
+
+   animate()
 }
    
