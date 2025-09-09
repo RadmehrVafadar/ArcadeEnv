9b36afdeb78d629d56cdfae6863ecbe58b2e596e
Radmehr <23xf1@queensu.ca>
2025-09-09 16:34:12 -0400

    The fix broke something else so reverted

--- src/arcade.jsx
+++ src/arcade.jsx
@@ -46,13 +46,9 @@ export default function Arcade() {
         state.camera.lookAt(0,1,1)
         
         if (hovered) {
-            // Zoom in when hovered
             state.camera.position.lerp(vec.set(0,3,3), 0.05)
-        } else {
-            // Zoom out to original position when not hovered
-            state.camera.position.lerp(vec.set(0,3,8), 0.05)
+            state.camera.updateProjectionMatrix()
         }
-        state.camera.updateProjectionMatrix()
         return null;
     })
 
